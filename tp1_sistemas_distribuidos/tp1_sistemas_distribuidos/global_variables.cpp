6bd2b854153e0013c9064cd3b4631952d73f4975
dontthinkaboutit <jpedaf@gmail.com>
2020-10-09 09:09:15 -0300

    Remove old project files to update them

--- tp1_sistemas_distribuidos/tp1_sistemas_distribuidos/global_variables.cpp
+++
@@ -1,9 +0,0 @@
-#include "global_variables.h"
-
-position_parameters_t positionParameters;
-loading_parameters_t loadingParameters;
-int countMessages = 0;
-char dataMsg[POSITION_MSG_LENGHT] = "55|NNNNN|NNNNN|NNNNN|NNNNN|NNNN.NN";
-char ackMsg[ACK_MSG_LENGHT] = "00|NNNNN";
-
-int seq_number = 0;
